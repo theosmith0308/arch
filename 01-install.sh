e47dcfd191c4a970f2f56d60b2ee6523401d1057
TheoSmith0308 <85744091+TheoSmith0308@users.noreply.github.com>
2023-08-29 20:12:09 +0200

    Update 01-install.sh

--- 01-install.sh
+++ 01-install.sh
@@ -62,7 +62,7 @@ mount /dev/$sda1 /mnt/boot/efi
 timedatectl set-ntp true
 pacman -S --noconfirm archlinux-keyring #update keyrings to latest to prevent packages failing to install
 pacman -S --noconfirm --needed pacman-contrib terminus-font
-setfont ter-v22b
+setfont ter-v20b
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 pacman -S --noconfirm --needed reflector rsync
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
