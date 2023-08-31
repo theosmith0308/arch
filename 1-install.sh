0f36607b9b519af2a0048b093a6d200badc247fa
TheoSmith0308 <85744091+TheoSmith0308@users.noreply.github.com>
2023-08-31 08:13:04 +0200

    Add files via upload

---
+++ 1-install.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+clear
+echo "    _             _       ___           _        _ _ "
+echo "   / \   _ __ ___| |__   |_ _|_ __  ___| |_ __ _| | |"
+echo "  / _ \ | '__/ __| '_ \   | || '_ \/ __| __/ _' | | |"
+echo " / ___ \| | | (__| | | |  | || | | \__ \ || (_| | | |"
+echo "/_/   \_\_|  \___|_| |_| |___|_| |_|___/\__\__,_|_|_|"
+echo ""
+echo "by Stephan Raabe (2023)"
+echo "-----------------------------------------------------"
+echo ""
+echo "Important: Please make sure that you have followed the "
+echo "manual steps in the README to partition the harddisc!"
+echo "Warning: Run this script at your own risk."
+echo ""
+
+# ------------------------------------------------------
+# Enter partition names
+# ------------------------------------------------------
+lsblk
+read -p "Enter the name of the EFI partition (eg. sda1): " sda1
+read -p "Enter the name of the ROOT partition (eg. sda2): " sda2
+# read -p "Enter the name of the VM partition (keep it empty if not required): " sda3
+
+# ------------------------------------------------------
+# Sync time
+# ------------------------------------------------------
+timedatectl set-ntp true
+
+# ------------------------------------------------------
+# Format partitions
+# ------------------------------------------------------
+mkfs.fat -F 32 /dev/$sda1;
+mkfs.btrfs -f /dev/$sda2
+# mkfs.btrfs -f /dev/$sda3
+
+# ------------------------------------------------------
+# Mount points for btrfs
+# ------------------------------------------------------
+mount /dev/$sda2 /mnt
+btrfs su cr /mnt/@
+btrfs su cr /mnt/@home
+btrfs su cr /mnt/@log
+btrfs su cr /mnt/@pkg
+btrfs su cr /mnt/@images
+umount /mnt
+
+mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@ /dev/$sda2 /mnt
+mkdir -p /mnt/{boot/efi,home,var/log,var/cache/pacman/pkg,var/lib/libvirt/images}
+mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@home /dev/$sda2 /mnt/home
+mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@log /dev/$sda2 /mnt/var/log
+mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@pkg /dev/$sda2 /mnt/var/cache/pacman/pkg
+mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@images /dev/$sda2 /mnt/var/lib/libvirt/images
+mount /dev/$sda1 /mnt/boot/efi
+# mkdir /mnt/windows
+# mount /dev/$sda3 /mnt/windows
+
+# ------------------------------------------------------
+# Setting up mirrors for optimal download
+# ------------------------------------------------------
+timedatectl set-ntp true
+pacman -S --noconfirm archlinux-keyring #update keyrings to latest to prevent packages failing to install
+pacman -S --noconfirm --needed pacman-contrib terminus-font
+setfont ter-v20b
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+sed -i 's/ParallelDownloads = 5/ParallelDownloads = 2/' /etc/pacman.conf
+# pacman -S --noconfirm --needed reflector rsync
+# cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+
+# ------------------------------------------------------
+# Run reflector to update mirrorlist
+# ------------------------------------------------------
+# reflector -c GB -c DE --sort rate -l 10 --save /etc/pacman.d/mirrorlist
+pacman -Sy
+
+# ------------------------------------------------------
+# Install base packages
+# ------------------------------------------------------
+pacstrap -K /mnt base base-devel linux linux-firmware intel-ucode btrfs-progs git vim reflector rsync pacman-contrib openssh
+
+# ------------------------------------------------------
+# Generate fstab
+# ------------------------------------------------------
+genfstab -U /mnt >> /mnt/etc/fstab
+cat /mnt/etc/fstab
+
+# ------------------------------------------------------
+# Install configuration scripts
+# ------------------------------------------------------
+mkdir /mnt/archinstall
+cp -r ./ /mnt/archinstall/
+
+# ------------------------------------------------------
+# Chroot to installed sytem
+# ------------------------------------------------------
+arch-chroot /mnt ./archinstall/2-configuration.sh
+
---
+++ 2-configuration.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+#   ____             __ _                       _   _             
+#  / ___|___  _ __  / _(_) __ _ _   _ _ __ __ _| |_(_) ___  _ __  
+# | |   / _ \| '_ \| |_| |/ _` | | | | '__/ _` | __| |/ _ \| '_ \ 
+# | |__| (_) | | | |  _| | (_| | |_| | | | (_| | |_| | (_) | | | |
+#  \____\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__|_|\___/|_| |_|
+#                         |___/                                   
+# (2023)
+# ------------------------------------------------------
+clear
+keyboardlayout="en-US"
+zoneinfo="Africa/Windhoek"
+hostname="arch"
+username="theo"
+
+# ------------------------------------------------------
+# Set System Time
+# ------------------------------------------------------
+ln -sf /usr/share/zoneinfo/$zoneinfo /etc/localtime
+hwclock --systohc
+
+# ------------------------------------------------------
+# Setting up reflector and mirrors for optimal download
+# ------------------------------------------------------
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+# sed -i 's/ParallelDownloads = 5/ParallelDownloads = 2/' /etc/pacman.conf
+pacman -S --noconfirm --needed reflector rsync
+# cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+echo "Start reflector..."
+reflector -c ZA -c US --sort rate -f 5 -l 10 --save /etc/pacman.d/mirrorlist
+
+# ------------------------------------------------------
+# Synchronize mirrors
+# ------------------------------------------------------
+pacman -Syy
+
+# ------------------------------------------------------
+# Install Packages
+# ------------------------------------------------------
+pacman -S --noconfirm --needed - < pkglist.txt 
+
+# ------------------------------------------------------
+# set lang utf8 US
+# ------------------------------------------------------
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# ------------------------------------------------------
+# Set Keyboard
+# ------------------------------------------------------
+echo "FONT=ter-v18n" >> /etc/vconsole.conf
+# echo "KEYMAP=$keyboardlayout" >> /etc/vconsole.conf
+
+# ------------------------------------------------------
+# Set hostname and localhost
+# ------------------------------------------------------
+echo "$hostname" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
+clear
+
+# ------------------------------------------------------
+# Set Root Password
+# ------------------------------------------------------
+echo "Set root password"
+passwd root
+
+# ------------------------------------------------------
+# Add User
+# ------------------------------------------------------
+echo "Add user $username"
+useradd -m -G wheel $username
+passwd $username
+
+# ------------------------------------------------------
+# Enable Services
+# ------------------------------------------------------
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable avahi-daemon
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable firewalld
+systemctl enable acpid
+
+# ------------------------------------------------------
+# Grub installation
+# ------------------------------------------------------
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch --removable
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# ------------------------------------------------------
+# Add btrfs and setfont to mkinitcpio
+# ------------------------------------------------------
+# Before: BINARIES=()
+# After:  BINARIES=(btrfs setfont)
+sed -i 's/BINARIES=()/BINARIES=(btrfs setfont)/g' /etc/mkinitcpio.conf
+mkinitcpio -p linux
+
+# ------------------------------------------------------
+# Add user to wheel
+# ------------------------------------------------------
+clear
+# Add sudo no password rights
+sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+
+# ------------------------------------------------------
+# Copy installation scripts to home directory 
+# ------------------------------------------------------
+mkdir -p /home/$username/scripts
+cp ./extra/* /home/$username/scripts
+
+clear
+echo "     _                   "
+echo "  __| | ___  _ __   ___  "
+echo " / _' |/ _ \| '_ \ / _ \ "
+echo "| (_| | (_) | | | |  __/ "
+echo " \__,_|\___/|_| |_|\___| "
+echo "                         "
+echo ""
+echo "Please find the following additional installation scripts in your home directory:"
+echo "- yay AUR helper: 3-yay.sh"
+echo "- zram swap: 4-zram.sh"
+echo "- timeshift snapshot tool: 5-timeshift.sh"
+echo "- preload application cache: 6-preload.sh"
+echo ""
+echo "Please exit & shutdown (shutdown -h now), remove the installation media and start again."
+echo "Important: Activate WIFI after restart with nmtui."
