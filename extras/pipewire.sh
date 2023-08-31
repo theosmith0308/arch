6c852b531fc370b4e04caf953aa60b70fd8bd2c3
TheoSmith0308 <85744091+TheoSmith0308@users.noreply.github.com>
2023-08-31 18:09:48 +0200

    Update pipewire.sh

--- extras/pipewire.sh
+++ extras/pipewire.sh
@@ -2,4 +2,6 @@
 # -----------------------------------------------------
 # Install pipewire
 # -----------------------------------------------------
-sudo pacman -S --needed gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly gstreamer pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-pulse pipewire-zeroconf playerctl volumeicon
+sudo pacman -S --needed gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly gstreamer pipewire pipewire-alsa pipewire-jack pipewire-pulse gst-plugin-pipewire ibpulse wireplumber
+
+systemctl enable --user pipewire-pulse.service
