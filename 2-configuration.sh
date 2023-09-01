49a996ccd57e1dbf5ddb6517ce231ff2e1955a4a
TheoSmith0308 <85744091+TheoSmith0308@users.noreply.github.com>
2023-09-01 21:12:44 +0200

    Update 2-configuration.sh

--- 2-configuration.sh
+++ 2-configuration.sh
@@ -51,8 +51,8 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 # ------------------------------------------------------
 # Set Keyboard
 # ------------------------------------------------------
-echo "FONT=ter-v20b" >> /etc/vconsole.conf
-echo "KEYMAP=$keyboardlayout" >> /etc/vconsole.conf
+# echo "FONT=ter-v20b" >> /etc/vconsole.conf
+# echo "KEYMAP=$keyboardlayout" >> /etc/vconsole.conf
 
 # ------------------------------------------------------
 # Set hostname and localhost
