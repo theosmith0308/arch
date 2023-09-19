d8c538157af6353b52f4d85ce71c9165971b4acd
TheoSmith0308 <85744091+TheoSmith0308@users.noreply.github.com>
2023-09-19 13:40:15 +0200

    Update 3-Cust-install.sh

--- 3-Cust-install.sh
+++ 3-Cust-install.sh
@@ -20,7 +20,7 @@ echo ""
 lsblk
 read -p "Enter the name of the EFI partition (eg. sda1): " sda1
 read -p "Enter the name of the ROOT partition (eg. sda2): " sda2
-read -p "Enter the name of the windows partition (keep it empty if not required): " sda3
+read -p "Enter the name of the ext4 Home partition (eg. sda3): " sda3
 
 # ------------------------------------------------------
 # Sync time
@@ -32,24 +32,23 @@ timedatectl set-ntp true
 # ------------------------------------------------------
 mkfs.fat -F 32 /dev/$sda1
 mkfs.btrfs -f /dev/$sda2
-# mkfs.btrfs -f /dev/$sda3
+mkfs.ext4 /dev/$sda3
 
 # ------------------------------------------------------
 # Mount points for btrfs
 # ------------------------------------------------------
 mount /dev/$sda2 /mnt
 btrfs su cr /mnt/@
-btrfs su cr /mnt/@home
 btrfs su cr /mnt/@var_log
 btrfs su cr /mnt/@snapshots
 umount /mnt
 
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@ /dev/$sda2 /mnt
 mkdir -p /mnt/{efi,home,var/log,.snapshots,.btrfsroot}
-mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@home /dev/$sda2 /mnt/home
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@var_log /dev/$sda2 /mnt/var/log
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvol=@snapshots /dev/$sda2 /mnt/.snapshots
 mount -o noatime,space_cache=v2,compress=zstd,ssd,discard=async,subvolid=5 /dev/$sda2 /mnt/.btrfsroot
+mount -o noatime,commit=120 /dev/$sda3 /mnt/home
 mount /dev/$sda1 /mnt/efi
 # mkdir /mnt/windows
 # mount /dev/$sda3 /mnt/windows
@@ -63,7 +62,7 @@ pacman -S --noconfirm --needed pacman-contrib terminus-font
 sed -i 's/^#Color/Color/' /etc/pacman.conf
 sed -i 's/^#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-sed -i 's/ParallelDownloads = 5/ParallelDownloads = 3/' /etc/pacman.conf
+# sed -i 's/ParallelDownloads = 5/ParallelDownloads = 3/' /etc/pacman.conf
 # pacman -S --noconfirm --needed reflector rsync
 # cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
