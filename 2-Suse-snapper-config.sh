eb3d9f7d7438bef1062e9e289fb328933aaf4cdc
TheoSmith0308 <85744091+TheoSmith0308@users.noreply.github.com>
2023-09-20 21:34:28 +0200

    Update 2-Suse-snapper-config.sh

--- 2-Suse-snapper-config.sh
+++ 2-Suse-snapper-config.sh
@@ -105,18 +105,14 @@ systemctl enable --now grub-btrfsd.service
 #Changing The timeline auto-snap
 sed -i 's|ALLOW_USERS=""|ALLOW_USERS="theo"|' /etc/snapper/configs/root
 sed -i 's|ALLOW_GROUPS=""|ALLOW_GROUPS="wheel"|' /etc/snapper/configs/root
-sed -i 's|NUMBER_LIMIT="50"|NUMBER_LIMIT="5-15"|' /etc/snapper/configs/root
-sed -i 's|NUMBER_LIMIT_IMPORTANT="50"|NUMBER_LIMIT_IMPORTANT="5-10"|' /etc/snapper/configs/root
-sed -i 's|TIMELINE_LIMIT_HOURLY="10"|TIMELINE_LIMIT_HOURLY="5"|' /etc/snapper/configs/root
-sed -i 's|TIMELINE_LIMIT_DAILY="10"|TIMELINE_LIMIT_DAILY="7"|' /etc/snapper/configs/root
-sed -i 's|TIMELINE_LIMIT_WEEKLY="0"|TIMELINE_LIMIT_WEEKLY="0"|' /etc/snapper/configs/root
-sed -i 's|TIMELINE_LIMIT_MONTHLY="10"|TIMELINE_LIMIT_MONTHLY="0"|' /etc/snapper/configs/root
-sed -i 's|TIMELINE_LIMIT_YEARLY="10"|TIMELINE_LIMIT_YEARLY="0"|' /etc/snapper/configs/root
+sed -i 's|NUMBER_LIMIT="50"|NUMBER_LIMIT="12"|' /etc/snapper/configs/root
+sed -i 's|NUMBER_LIMIT_IMPORTANT="50"|NUMBER_LIMIT_IMPORTANT="3"|' /etc/snapper/configs/root
+sed -i 's|TIMELINE_CREATE="yes|TIMELINE_CREATE="no"|' /etc/snapper/configs/root
 
 #activating the auto-cleanup
 SCRUB=$(systemd-escape --template btrfs-scrub@.timer --path /dev/sda5)
 systemctl enable ${SCRUB}
-systemctl enable snapper-timeline.timer
+systemctl disable snapper-timeline.timer
 systemctl enable snapper-cleanup.timer
 sed -i 's|OnUnitActiveSec=1d|OnUnitActiveSec=1h|' /etc/systemd/system/timers.target.wants/snapper-cleanup.timer
 grub-mkconfig -o /boot/grub/grub.cfg
