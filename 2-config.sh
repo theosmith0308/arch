1f42c2d616185d46d3d4ffb8776dd0b5de80b8f9
Theo Smith <85744091+theosmith0308@users.noreply.github.com>
2024-06-18 23:06:08 +0200

    Update 2-config.sh

--- 2-config.sh
+++ 2-config.sh
@@ -116,8 +116,7 @@ sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 # ------------------------------------------------------
 # Copy installation scripts to home directory 
 # ------------------------------------------------------
-mkdir -p /home/$username/extras
-sudo cp -r /archinstall/extras/ /home/$username/extras/
+sudo cp -r /archinstall/extras /home/$username/
 sudo chown -R $username /home/$username/extras/
 sudo chmod +x /home/$username/extras/*
 
