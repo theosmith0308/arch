bab13506a36d2e121c9ce43f24bf8d952061a4c2
Theo Smith <85744091+theosmith0308@users.noreply.github.com>
2024-06-21 09:07:20 +0200

    Update 2-config.sh

--- 2-config.sh
+++ 2-config.sh
@@ -83,7 +83,7 @@ systemctl enable NetworkManager
 # systemctl enable bluetooth
 # systemctl enable cups.service
 systemctl enable sshd
-# systemctl enable avahi-daemon
+systemctl enable avahi-daemon
 # systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable firewalld
@@ -94,7 +94,6 @@ systemctl enable acpid
 # ------------------------------------------------------
 grub-install --target=x86_64-efi --efi-directory=/efi --boot-directory=/boot --bootloader-id=Arch
 grub-mkconfig -o /boot/grub/grub.cfg
-# bootctl install
 
 # ------------------------------------------------------
 # Add btrfs and setfont to mkinitcpio
