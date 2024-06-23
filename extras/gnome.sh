62989e386df9fb27b2ba6f3872a9219597ba5cfc
Theo Smith <85744091+theosmith0308@users.noreply.github.com>
2024-06-23 22:33:12 +0200

    Update gnome.sh

--- extras/gnome.sh
+++ extras/gnome.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------
 # Install gnome desktop
 # -----------------------------------------------------
-sudo pacman -S --noconfirm --needed baobab dconf-editor eog evince file-roller gdm ghex gitg glade gnome-backgrounds gnome-browser-connector gnome-calculator gnome-characters gnome-color-manager gnome-connections gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs gnome-menus gnome-photos gnome-remote-desktop gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-text-editor gnome-tweaks gnome-user-docs gnome-user-share grilo-plugins gvfs gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb nautilus rygel sushi xdg-desktop-portal-gnome xdg-user-dirs-gtk
+sudo pacman -S --noconfirm --needed baobab dconf-editor eog evince file-roller gdm ghex gitg glade gnome-backgrounds gnome-browser-connector gnome-calculator gnome-characters gnome-color-manager gnome-connections gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs gnome-menus gnome-photos gnome-remote-desktop gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-console gnome-text-editor gnome-tweaks gnome-user-docs gnome-user-share grilo-plugins gvfs gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb nautilus rygel sushi xdg-desktop-portal-gnome xdg-user-dirs-gtk
 # -----------------------------------------------------
 # Enable gdm
 # -----------------------------------------------------
